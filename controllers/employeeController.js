87bbe67167e9bba5efbbccd47a7f92557243aa35
rhsb <ovillafana65@gmail.com>
2023-01-11 11:38:18 -0800

    third commit

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,6 +1,34 @@
 const {Employee} = require('../models')
+const jobtitles = ['CEO', 'VIP']
+const states = ['CA', 'AZ']
 
-module.exports.displayEmployee = async function(req, res){
+module.exports.displayEmployees = async function(req, res){
     const employees = await Employee.findAll();
     res.render('index', {employees})
+}
+
+module.exports.renderAddEmployeeForm = function(req,res){
+    res.render('createUserForm',
+        {
+            jobtitles,
+            stateslist:states
+    });
+}
+
+module.exports.addEmployee = async function(req,res){
+    await Employee.create(
+        {
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            jobtitles:req.body.jobtitles,
+            streetline1:req.body.streetline1,
+            streetline2:req.body.streetline2,
+            city:req.body.city,
+            state:req.body.state,
+            zip:req.body.zip,
+            phonenumber:req.body.phonenumber,
+            yearhired:req.body.yearhired
+        }
+    );
+    res.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const employeeController = require('../controllers/employeeController')
 
 /* GET home page. */
-router.get('/', employeeController.displayEmployee)
+router.get('/', employeeController.displayEmployees)
+router.get('/add', employeeController.renderAddEmployeeForm)
 
 module.exports = router;
