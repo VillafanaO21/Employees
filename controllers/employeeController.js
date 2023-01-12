94243e678942ae693bf44f18d9eb2f65bba08b6e
rhsb <ovillafana65@gmail.com>
2023-01-12 11:37:30 -0800

    4 commit

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -32,3 +32,34 @@ module.exports.addEmployee = async function(req,res){
     );
     res.redirect('/');
 }
+
+module.exports.renderUpdateForm = async function(req,res){
+    const employee = await Employee.findByPk( req.params.id);
+    res.render('edit', {
+        employee,
+        jobtitles,
+        stateslist:states
+    });
+}
+
+module.exports.updateEmployee = async function(req, res){
+    await Employee.update(
+               {
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            jobtitle:req.body.jobtitle,
+            streetline1:req.body.streetline1,
+            streetline2:req.body.streetline2,
+            city:req.body.city,
+            state:req.body.state,
+            zip:req.body.zip,
+            phonenumber:req.body.phonenumber,
+            yearhired:req.body.yearhired
+        },
+            {
+            where: {
+                id: req.params.id
+            }
+        });
+    res.redirect('/')
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const employeeController = require('../controllers/employeeController')
 
 /* GET home page. */
-router.get('/', employeeController.displayEmployees)
-router.get('/add', employeeController.renderAddEmployeeForm)
-
+router.get('/', employeeController.displayEmployees);
+router.get('/add', employeeController.renderAddEmployeeForm);
+router.get('/add', employeeController.addEmployee);
+router.get('/update/:id', employeeController.renderUpdateForm);
+router.post('/update/:id', employeeController.updateEmployee);
 module.exports = router;
