8dd6365d034aa6ae0c4bdbc65765dc50099f8112
rhsb <ovillafana65@gmail.com>
2023-01-19 11:33:06 -0800

    6 commit

--- models/employee.js
+++ models/employee.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
     zip: DataTypes.INTEGER,
     phonenumber: DataTypes.INTEGER,
     yearhired: DataTypes.INTEGER,
+    salary: DataTypes. INTEGER
   }, {
     sequelize,
     modelName: 'Employee',
